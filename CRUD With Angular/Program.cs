9f501587163deba5726e8f36c2d6043c27df69a8
Viththiyakaran <battikaran6@gmail.com>
2023-02-27 22:24:57 +0530

    Add cors

--- CRUD With Angular/Program.cs
+++ CRUD With Angular/Program.cs
@@ -1,6 +1,7 @@
 using CRUD_With_Angular.Database;
 using CRUD_With_Angular.Interface;
 using CRUD_With_Angular.Repo;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,14 @@ builder.Services.AddScoped<IEmployee, EmployeeService>();
 
 builder.Services.AddDbContext<AppDbContext>( options => options.UseSqlServer(builder.Configuration.GetConnectionString("ServerCon")));
 
+builder.Services.AddCors((CorsOptions) =>
+{
+    CorsOptions.AddPolicy("MyPolicy", (corsOpt) => {
+
+        corsOpt.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+
+app.UseCors("MyPolicy");
 
 app.MapControllers();
 
